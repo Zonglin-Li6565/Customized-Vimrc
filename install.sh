333d374bffe975305810a007246bc27e7f35867b
Zonglin Li <lizonglin6565@163.com>
2017-09-04 14:51:35 -0500

    updated install script

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Zonglin-Li6565/myvimrc.git ~/Downloads/myvimrc
-git clone https://github.com/powerline/fonts.git ~/Downloads/powerlinefont
-cp ~/Download/myvimrc/vimrc ~/.vimrc
+git clone https://github.com/Zonglin-Li6565/myvimrc.git ~/temp/myvimrc
+git clone https://github.com/powerline/fonts.git ~/temp/powerlinefont
+cp ~/temp/myvimrc/vimrc ~/.vimrc
 vim +PluginInstall
-cd ~/Downloads/powerlinefont
-./install.sh
+bash ~/temp/powerlinefont/install.sh
+rm -rf ~/temp
